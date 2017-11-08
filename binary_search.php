59dc73a385b48df1e1e787080a11d8811f00551b
amykhalchyshyn <a.mykhalchyshyn@gmail.com>
2017-11-08 21:45:22 +0200

    binary search final

--- binary_search.php
+++ binary_search.php
@@ -1,7 +1,7 @@
 <?php
 function binary_search(array $haystack, int $needle, int $start, int $end) : int
 {
-	if ($start - $end == 0 && $haystack[$start] != $needle)  {
+	if($needle > $haystack[$end] || $needle < $haystack[$start]) {
 		return -1;
 	}
 	$middle = floor(($end + $start)/2);
@@ -9,13 +9,11 @@ function binary_search(array $haystack, int $needle, int $start, int $end) : int
 		return $middle;
 	}
 	if($haystack[$middle] > $needle) {
-		echo $middle . 'ddd' . PHP_EOL;
 		return binary_search($haystack, $needle, 0, $middle - 1);
 	} else {
-		echo $middle . 'qqq' . PHP_EOL;
 		return binary_search($haystack, $needle, $middle + 1, count($haystack) - 1);
 	}
 }
 
 $array = [1,2,3,6,7,9,11];
-echo binary_search($array, 4, 0, count($array) -1);
+echo binary_search($array, 7, 0, count($array) -1);
